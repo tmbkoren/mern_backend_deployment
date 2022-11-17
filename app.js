5590981db34d994b5f9074c5149115d22c8c9922
Vladyslav Korenevskyi <25588519+tmbkoren@users.noreply.github.com>
2022-11-17 15:15:50 -0800

    Deployment fix

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((err, req, res, next) => {
 
 mongoose
   .connect(
-    ` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hvmsolc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hvmsolc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => app.listen(5000))
   .catch((err) => console.log(err));
